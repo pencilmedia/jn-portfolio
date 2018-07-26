0f82662dc60cf7927d502c0046fb9c304cad3d08
ralph <ralph@pencilmedia.com>
2018-07-26 00:09:37 -0700

    nav

--- app/inc/nav.inc.php
+++ app/inc/nav.inc.php
@@ -1,5 +1,10 @@
 <nav class="nav nav--global">
-    <a href="#">About</a>
-    <a href="index.php">Projects</a>
-    <a href="details.php">Process</a>
+    <a href="index.php" class="nav--profile">
+        <img src="assets/images/photo_jn.png" alt="Joel Nave: Product Design Leader" height="38px" class="nav--photo" />
+    </a>
+    <div class="nav--items">
+        <a href="#" class="nav--item">About</a>
+        <a href="index.php" class="nav--item">Projects</a>
+        <a href="details.php" class="nav--item">Process</a>
+    </div>
 </nav>
